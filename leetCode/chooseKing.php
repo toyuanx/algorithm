edc888e1cf3e5810f0521fb0f8b231e39da6b650
toyuanx <564498011@qq.com>
2019-12-02 19:01:52 +0800

    猴子选大王

---
+++ leetCode/chooseKing.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * 猴子选大王
+ *
+ * User: 原子酱
+ * Date: 2019/12/2
+ * Time: 18:26
+ */
+
+function chooseKing($nums)
+{
+    if (count($nums) == 1) {
+        return current($nums);
+    }
+
+    foreach ($nums as $key => $value) {
+        (($key + 1) % 3 != 0) && array_push($nums, $value);
+        unset($nums[$key]);
+    }
+
+    return chooseKing($nums);
+}
+
+function buildArray($num)
+{
+    return range(1, $num);
+}
+
+var_dump(chooseKing(buildArray(11)));
